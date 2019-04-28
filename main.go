c7325c66c0ceef420e417810473df12b3a4bc48f
Mario Medeiros <mariomedeiros@gmail.com>
2019-04-28 18:17:45 +0000

    mudanças do index.html

--- main.go
+++ main.go
@@ -5,25 +5,45 @@ import (
 	"log"
 	"net/http"
 	"os"
+
+	"github.com/gorilla/mux"
+)
+
+const (
+	appName    = "svc-api"
+	appVersion = "0.0.1-alfa001"
+	httpPort   = "8080"
+	topicName  = "topicNotifification"
+	projectID  = "xallcloud"
 )
 
 func main() {
-	http.HandleFunc("/api", apiHandler)
 
 	port := os.Getenv("PORT")
 	if port == "" {
-		port = "8080"
-		log.Printf("Defaulting to port %s", port)
+		port = httpPort
+		log.Printf("Service: %s. Defaulting to port %s", appName, port)
 	}
 
-	log.Printf("Listening on port %s", port)
-	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+	router := mux.NewRouter()
+
+	router.HandleFunc("/api/version", getVersionHanlder).Methods("GET")
+	router.HandleFunc("/api/callpoints", getCallpointsHanlder).Methods("GET")
+
+	log.Printf("Service: %s. Listening on port %s", appName, port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), router))
 }
 
-func apiHandler(w http.ResponseWriter, r *http.Request) {
-	if r.URL.Path != "/api" {
-		http.NotFound(w, r)
-		return
-	}
-	fmt.Fprint(w, `{"name": "xallcloud", "version": "0.1.0"}`)
+func getVersionHanlder(w http.ResponseWriter, r *http.Request) {
+	log.Println("[/version:GET] Requested api version.")
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, fmt.Sprintf(`{"service": "%s", "version": "%s"}`, appName, appVersion))
+}
+
+func getCallpointsHanlder(w http.ResponseWriter, r *http.Request) {
+	log.Println("[/callpoints:GET] Requested get all callpoints")
+
+	w.Header().Set("Content-Type", "application/json")
+	fmt.Fprint(w, `{"callpoints": {} }`)
 }
