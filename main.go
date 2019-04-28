03959195c59b3cfb6c6e263800c18633210a63ed
Mario Medeiros <mariomedeiros@gmail.com>
2019-04-28 16:11:24 +0000

    cleaned up comments

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
-// Copyright 2019 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//     https://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-// [START gae_go111_app]
-
-// Sample helloworld is an App Engine app.
 package main
 
-// [START import]
 import (
 	"fmt"
 	"log"
@@ -25,14 +7,9 @@ import (
 	"os"
 )
 
-// [END import]
-// [START main_func]
-
 func main() {
 	http.HandleFunc("/api", apiHandler)
-	//http.HandleFunc("/", indexHandler)
 
-	// [START setting_port]
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8080"
@@ -41,14 +18,7 @@ func main() {
 
 	log.Printf("Listening on port %s", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
-	// [END setting_port]
 }
-
-// [END main_func]
-
-// [START indexHandler]
-
-// indexHandler responds to requests with our greeting.
 
 func apiHandler(w http.ResponseWriter, r *http.Request) {
 	if r.URL.Path != "/api" {
@@ -57,6 +27,3 @@ func apiHandler(w http.ResponseWriter, r *http.Request) {
 	}
 	fmt.Fprint(w, `{"name": "xallcloud", "version": "0.1.0"}`)
 }
-
-// [END indexHandler]
-// [END gae_go111_app]
